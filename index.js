04cfbc9f9a23c70733750834bbd3ed6366aff904
Ashish Gurram <ashishchinnu0@gmail.com>
2024-10-25 19:57:58 +0530

    Second Commit- Review

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-let x = 10;
-console.log(x);
+import express from "express";
+import userRouter from "./routes/userRoutes.js";
+import mongoose from "mongoose";
+import cors from "cors";
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.use("/users", userRouter);
+
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/ecommdb")
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server Started on port 8080");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
