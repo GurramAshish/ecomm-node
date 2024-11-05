2e9735f12bca8486329774ae786313775d75721a
Ashish Gurram <ashishchinnu0@gmail.com>
2024-11-05 20:18:41 +0530

    Need to give functionallity for the button!!

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ import express from "express";
 import userRouter from "./routes/userRoutes.js";
 import mongoose from "mongoose";
 import cors from "cors";
+import productRouter from "./routes/productsRoutes.js";
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 
 app.use("/users", userRouter);
-
+app.use("/products", productRouter)
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/ecommdb")
